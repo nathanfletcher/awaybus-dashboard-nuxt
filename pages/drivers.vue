626ba1a735a15f18885bae08c296ec70aa642bca
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2023-07-17 10:25:53 +0000

    minor update

--- pages/drivers.vue
+++ pages/drivers.vue
@@ -5,11 +5,19 @@
           :headers="tableHeaders"
           :items="drivers"
           :items-per-page="itemsPerPage"
+          :items-length="totalItems"
+          :loading="loading"
+          :item-value="drivers"
           :page="currentPage"
-          :items-length="driverLength()"
           @update:options="getAllDrivers"
         >
-            <template v-slot:item.isVerified="{ item }">
+        <template v-slot:item.created_at>
+            <p>{{  date.format('2023-01-04T23:05:06.299989+00:00', 'normalDateWithWeekday') }}</p>
+        </template>
+        <template v-slot:item.lastActive>
+            <p>{{ date.format('2023-01-04T23:05:06.299989+00:00', 'normalDateWithWeekday') }}</p>
+        </template>
+            <!--<template v-slot:item.isVerified="{ item }">
                 <v-chip
                 :color="item.isVerified ? 'green' : 'red'"
                 dark
@@ -33,13 +41,16 @@
                 mdi-delete
                 </v-icon>
             </template>
-            <template v-slot:no-data>
+             <template v-slot:no-data>
                 <v-btn
                 color="primary"
                 @click="initialize"
                 >
                 Reset
                 </v-btn>
+            </template> -->
+            <template v-slot:bottom>
+                <v-pagination v-model="itemsPerPage" @update:model-value="currentPage = parseInt($event, +10)"></v-pagination>
             </template>
         </v-data-table-server>
 
@@ -48,17 +59,20 @@
 </template>
 <script setup>
     import { onMounted } from 'vue';
+    import { useDate } from 'vuetify/labs/date'
     import { VDataTable, VDataTableServer } from 'vuetify/labs/VDataTable'
     definePageMeta({
         middleware: 'auth'
     })
+    const date = useDate()
     const tableHeaders = ref([])
     const client = useSupabaseClient()
     const user = useSupabaseUser()
-    const itemsPerPage = ref(10)
+    const itemsPerPage = ref(5)
     const currentPage = ref(1)
-    const totalItems = ref(0)
+    //const totalItems = ref(0)
     const drivers = ref([])
+    const loading = ref(false)
     
     // const { data:drivers, error } = await useAsyncData('drivers', async () => {
     //     return await client.from('awayBusDrivers').select().order('created_at')
@@ -71,6 +85,13 @@
         return data;
     }) */
     //drivers.value = await client.from('awayBusDrivers').select().order('created_at').data;
+
+    // get count of all drivers using useAsyncData
+    const {data:totalItems, pending, error} =  await useAsyncData('awayBusDriversCount', async () => {
+        const { data, count,error } = await client.from('awayBusDrivers').select('*',{count:'exact'})
+        console.log('total count ',count)
+        return count;
+    })
     
 
     watchEffect(async () => {
@@ -80,12 +101,16 @@
     });
 
     async function getAllDrivers() {
-        return await useAsyncData('awayBusDrivers', async () => {
-        const { data } = await client.from('awayBusDrivers').select().order('created_at')
-        // console.log(data)
-        drivers.value = data;
-        return data;
-    })
+        console.log('this is page', currentPage.value) 
+        loading.value = true;
+        let result =  await useAsyncData('awayBusDrivers', async () => {
+            const { data } = await client.from('awayBusDrivers').select().range(currentPage.value-1,(currentPage.value ) * itemsPerPage.value)//.range(currentPage.value, currentPage.value+itemsPerPage.value)//.limit(itemsPerPage.value).range((currentPage.value - 1) * itemsPerPage.value).order('created_at', { ascending: false })
+            // console.log(data)
+            drivers.value = data;
+            return data;
+        })
+        loading.value = false;
+        return result;
     }
 
     const driverLength = () => {
