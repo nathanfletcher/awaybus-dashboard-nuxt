971aa49c1d67c1f83084fae14d14ee0aa5976492
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2023-09-15 10:44:54 +0000

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false,
+  devtools: { enabled: false },
+  ssr: true,
   modules: ['@nuxtjs/supabase','@invictus.codes/nuxt-vuetify',],
 })
